d707bd68f594488bdae7fa1535335bdb019917b6
SorinEni <60381859+SorinEni@users.noreply.github.com>
2021-02-13 12:51:57 +0100

    Add files via upload

---
+++ wp3-01/functions.php
@@ -0,0 +1,26 @@
+<?php
+
+function getCarDescription():void {
+$year = 1969;
+
+switch ($year) {
+    case $year < 1960: 
+        echo "To je fakt veterán!";
+        break; 
+    case $year < 1990: 
+        echo "Auto by na silnici už raději být nemělo";
+        break;
+    case $year < 2000: 
+        echo "Budete platit velkou silniční daň i povinné ručení";
+        break;
+    case $year < 2010: 
+         echo "Navštěvujte servis pravidelně" ;
+        break; 
+    default:
+        echo "Auto je způsobilé";  
+
+} 
+
+}
+
+?>
---
+++ wp3-01/index.php
@@ -0,0 +1,14 @@
+<?php
+require_once('functions.php');
+?>
+
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+  
+    <?php  getCarDescription() ?> 
+
+  </body>
+</html>
